c082e5f156011e0ceff461b361398740310b113c
Your Name <you@example.com>
2019-07-05 20:43:52 +0800

    7月5日课后作业

---
+++ 7.5_Morse_by_tinkercad/Morse_by_tinkercad.ino
@@ -0,0 +1,107 @@
+#include "Arduino.h"
+
+int _dottime=250;
+int pin=13;
+
+void morse_dot()
+{
+	digitalWrite(pin,HIGH);
+	delay(_dottime);
+	digitalWrite(pin,LOW);
+	delay(_dottime);
+}
+
+void morse_dash()
+{
+	digitalWrite(pin,HIGH);
+	delay(_dottime*4);
+	digitalWrite(pin,LOW);
+	delay(_dottime);
+}
+
+void morse_c_space()
+{
+	digitalWrite(pin,LOW);
+	delay(_dottime*3);
+}
+
+void morse_w_space()
+{
+	digitalWrite(pin,LOW);
+	delay(_dottime*7);
+}
+
+char MORSE[27][4] = {   //摩尔斯表供查询
+  {'.', '-', '*', '*'}, //A
+  {'-', '.', '.', '.'}, //B
+  {'-', '.', '-', '.'}, //C
+  {'-', '.', '.', '*'}, //D
+  {'.', '*', '*', '*'}, //E
+  {'.', '.', '-', '.'}, //F
+  {'-', '-', '.', '*'}, //G
+  {'.', '.', '.', '.'}, //H
+  {'.', '.', '*', '*'}, //I
+  {'.', '-', '-', '-'}, //J
+  {'-', '.', '-', '*'}, //K
+  {'.', '-', '.', '.'}, //L
+  {'-', '-', '*', '*'}, //M
+  {'-', '.', '*', '*'}, //N
+  {'-', '-', '-', '*'}, //O
+  {'.', '-', '-', '.'}, //P
+  {'-', '-', '.', '-'}, //Q
+  {'.', '-', '.', '*'}, //R
+  {'.', '.', '.', '*'}, //S
+  {'-', '*', '*', '*'}, //T
+  {'.', '.', '-', '*'}, //U
+  {'.', '.', '.', '-'}, //V
+  {'.', '-', '-', '*'}, //W
+  {'-', '.', '.', '-'}, //X
+  {'-', '.', '-', '-'}, //Y
+  {'-', '-', '.', '.'}, //Z
+  {'*', '*', '*', '*'}, //空格
+};
+
+void setup()
+{
+  pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  String str = ""; 
+  String morse_s = "";  
+  int i, t, temp = 0;
+  int n = 0; 
+  while (Serial.available() > 0)
+  {
+    temp = 1;  
+    str += char(Serial.read());  
+    delay(2); 
+    n++;
+  }
+
+  if (temp)
+  {
+    for (i = 0; i < n; i++)
+    {
+      if (str[i] >= 97 && str[i] <= 122)
+      {
+        for (t = 0; t < 4; t++)
+        {
+          morse_s += char(MORSE[int(str[i] - 97)][t]);
+        }
+      }
+      morse_s += ' ';
+    }
+    Serial.println(morse_s); 
+    for (i = 0; morse_s[i] != '\0' ; i++)
+    {
+      if (morse_s[i] == '.')morse_dot();
+      else if (morse_s[i] == '-')morse_dash();
+      else if (morse_s[i] == ' ')morse_w_space();
+      if (morse_s[i] != ' ' && str[i] != '*')morse_c_space();
+    }
+    delay(2);
+  }
+}
